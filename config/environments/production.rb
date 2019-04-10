a12c3529021eb8bb0a631a77f622328101ad016b
JaymeeDEV <jamiekellybrown@gmail.com>
2019-04-10 20:57:07 +0100

    Production ready

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://message-me-action-cable-app.herokuapp.com/cable" 
+
+  config.action_cable.allowed_request_origins = ['https://message-me-action-cable-app.herokuapp.com', 'http://message-me-action-cable-app.herokuapp.com']
 end
