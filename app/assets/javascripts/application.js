72246c8ae0ffb295265f572728c8595b70325cfa
JaymeeDEV <jamiekellybrown@gmail.com>
2019-04-10 20:22:59 +0100

    Enable autoscrolling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,12 @@
 //= require semantic-ui
 //= require_tree .
 
+scroll_bottom = function() {
+  if ($("#messages").length > 0) {
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
+  }
+};
+
 $(document).on("turbolinks:load", function() {
   $(".ui.dropdown").dropdown();
   $(".message .close").on("click", function() {
@@ -24,4 +30,5 @@ $(document).on("turbolinks:load", function() {
       .closest(".message")
       .transition("fade");
   });
+  scroll_bottom();
 });
