01287b49830be2b45117605ac3c956fbf3eb3cf7
JaymeeDEV <jamiekellybrown@gmail.com>
2019-04-10 20:27:04 +0100

    Beautify input box, functionality and create custom scope

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,6 +23,15 @@ scroll_bottom = function() {
   }
 };
 
+submit_message = function() {
+  $("#message_body").on("keydown", function(e) {
+    if (e.keyCode == 13) {
+      $("button").click();
+      e.target.value = "";
+    }
+  });
+};
+
 $(document).on("turbolinks:load", function() {
   $(".ui.dropdown").dropdown();
   $(".message .close").on("click", function() {
@@ -30,5 +39,6 @@ $(document).on("turbolinks:load", function() {
       .closest(".message")
       .transition("fade");
   });
+  submit_message();
   scroll_bottom();
 });
