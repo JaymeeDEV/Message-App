4fed270c8946080fa80a3d7696e9c4eecd8c71b9
JaymeeDEV <jamiekellybrown@gmail.com>
2019-04-10 18:25:50 +0100

    Post messages to the chatroom

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
 
   def new
   end
-
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
@@ -15,7 +14,6 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
-
   def destroy
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
@@ -29,5 +27,6 @@ class SessionsController < ApplicationController
       flash[:error] = "You are already logged in"
       redirect_to root_path
     end
+  end
 
 end
